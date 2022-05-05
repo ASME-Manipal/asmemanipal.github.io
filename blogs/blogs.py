c18aa2c4e3ed820fe1d13e0fcd58e18171296fe0
RishabhRishiRishu <rishabhrishirishu@gmail.com>
2022-05-05 20:39:20 +0530

    .

--- blogs/blogs.py
+++ blogs/blogs.py
@@ -1,18 +1,43 @@
 import pandas as pd
+from save_image import download_file_from_google_drive
 
 df = pd.read_csv("https://docs.google.com/spreadsheets/d/1TdIJZ77Ihs9MzzOjlUh_Mv7GeamsHF6GU1kE9hvZ5mU/export?format=csv&gid=0")
 
 df = df.sort_values(by='Index')
 
+blog_template = ""
+with open('blog_template.html') as inp_file:
+    blog_template = inp_file.read()
+
 final_text = ""
 for _, blog in df.iterrows():
-    print(blog)
-    final_text += f"""<div class="blog">
-            <img src="images/blogs/blog1.webp">
-            <p>{blog['Name']}</p>
-            <p>{blog['Author']}</p>
-            <p>{blog['Short Description']}</p>
-    </div>"""
+    # save Image
+    image_location = f"images/{blog['Name']}.png"
+    # download_file_from_google_drive(blog['Image Link'], image_location)
+
+    # make blog file
+    blog_html = blog_template
+    blog_html = blog_html.replace("{{Head}}", blog['Name'])
+    blog_html = blog_html.replace("{{Image}}", '../'+image_location)
+    blog_html = blog_html.replace("{{Content}}", blog['Content'].replace('\n', '<br>'))
+    blog_html = blog_html.replace("{{Author}}", blog['Author'])
+
+    file_name = f"blogs/{blog['Name']}.html"
+    with open(file_name, 'w') as out_file:
+        out_file.write(blog_html)
+
+    # add to blogs
+    final_text += f"""
+        <a href="{file_name}">
+            <div class="blog">
+                <img src="{image_location}">
+                <p>{blog['Name']}</p>
+                <p>{blog['Author']}</p>
+                <p>{blog['Short Description']}</p>
+            </div>
+        </a>
+        """
+
 
 with open('blogs_template.html', 'r') as inp_file:
     with open('blogs.html', 'w') as out_file:
